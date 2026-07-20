f24b304fc900aa7a5b121c56d8d022a7e875ee30
8csckyt9sz-dev <8csckyt9sz@privaterelay.appleid.com>
2026-07-20 12:33:33 +0900

    Create config.ts

---
+++ migrations/src/config.ts
@@ -0,0 +1,2 @@
+export const store={name:'マナエンタープライズ（株）',english:'MANA ENTERPRISES',phone:'Instagram（アカウント準備中）',hours:'8:30〜17:30',address:'兵庫県内（住所準備中）'};
+export const yen=(n:number)=>`${new Intl.NumberFormat('ja-JP').format(n||0)}円`;
